7b04b0f66fab35b5a2f66496ac18ffa5a22c8218
Loris-Gr <grandchamploris@gmail.com>
2024-10-18 15:12:59 +0200

    changement script création

--- BD/creation_grand_galop.sql
+++ BD/creation_grand_galop.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS RESERVER;
 DROP TABLE IF EXISTS COURS;
-DROP TABLE IF EXISTS PERSONNE;
+DROP TABLE IF EXISTS ADHERENT;
+DROP TABLE IF EXISTS MONITEUR;
 DROP TABLE IF EXISTS PONEY;
 
 CREATE TABLE PONEY (
@@ -10,35 +11,45 @@ CREATE TABLE PONEY (
     PRIMARY KEY(id_po)
 );
 
-CREATE TABLE PERSONNE (
-    id_pe INT,
-    nom_pe VARCHAR(42),
-    prenom_pe VARCHAR(42),
-    age INT,
-    categorie VARCHAR(10),
+CREATE TABLE ADHERENT (
+    id_a INT,
+    nom_a VARCHAR(42),
+    prenom_a VARCHAR(42),
+    date_de_naissance DATE,
     poids DECIMAL(3,2),
     email VARCHAR(42),
-    PRIMARY KEY (id_pe)
+    PRIMARY KEY (id_a)
+);
+
+CREATE TABLE MONITEUR (
+    id_m INT,
+    nom_m VARCHAR(42),
+    prenom_m VARCHAR(42),
+    date_de_naissance DATE,
+    poids DECIMAL(3,2),
+    email VARCHAR(42),
+    PRIMARY KEY (id_m)
 );
 
 CREATE TABLE COURS (
     id_c INT,
-    id_pe INT,
+    id_m INT,
     nb_pe INT CHECK (nb_pe <= 10 AND nb_pe >= 1),
     h_de_debut INT,
     duree INT CHECK (duree = 1 OR duree = 2),
     date_c DATE,
+    prix DECIMAL(5,2),
     PRIMARY KEY (id_c)
 );
 
 CREATE TABLE RESERVER(
-    id_pe INT,
+    id_a INT,
     id_po INT,
     id_c INT,
-    PRIMARY KEY (id_pe,id_po,id_c)
+    PRIMARY KEY (id_a,id_po,id_c)
 );
 
-ALTER TABLE COURS ADD FOREIGN KEY (id_pe) REFERENCES PERSONNE (id_pe);
-ALTER TABLE RESERVER ADD FOREIGN KEY (id_pe) REFERENCES PERSONNE (id_pe);
+ALTER TABLE COURS ADD FOREIGN KEY (id_m) REFERENCES MONITEUR (id_m);
+ALTER TABLE RESERVER ADD FOREIGN KEY (id_a) REFERENCES ADHERENT (id_a);
 ALTER TABLE RESERVER ADD FOREIGN KEY (id_po) REFERENCES PONEY (id_po);
 ALTER TABLE RESERVER ADD FOREIGN KEY (id_c) REFERENCES COURS (id_c);

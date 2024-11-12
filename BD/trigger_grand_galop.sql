79577480cb93e967542d7e225db23b71f912259f
DanCoquerant <dan.coquerant@gmail.com>
2024-11-12 18:20:08 +0100

    fin implementation du trigger pour chevauchement (insert+update)

--- BD/trigger_grand_galop.sql
+++ BD/trigger_grand_galop.sql
@@ -62,10 +62,10 @@ begin
 end|
 delimiter ;
 
---Trigger vérifiant si les cours d'une personne ne se chevauchent pas
+--Trigger vérifiant si les cours d'un adhérant ne se chevauchent pas (insert)
 
 delimiter |
-CREATE OR REPLACE TRIGGER pasDeCheuvauchement before insert on RESERVER for each row
+CREATE OR REPLACE TRIGGER pasDeCheuvauchementAdherantInsert before insert on RESERVER for each row
 begin
     DECLARE heureAvant INT;
     DECLARE heureApres INT;
@@ -75,27 +75,70 @@ begin
     DECLARE dureeCours INT;
     DECLARE dateCours INT;
 
+    -- on recupère les donnée sur le cour
     SELECT h_de_debut, duree, date_c INTO heureCours, dureeCours, dateCours FROM COURS WHERE id_c = new.id_c;
     
+    -- on récupère les info du plus proche cours précédent
     SELECT h_de_debut,duree INTO heureAvant, dureeAvant FROM RESERVER NATURAL JOIN COURS 
     WHERE date_c = dateCours AND h_de_debut < heureCours AND id_a = new.id_a
     ORDER BY h_de_debut DESC LIMIT 1;
 
-    SELECT h_de_debut FROM RESERVER NATURAL JOIN COURS 
-    WHERE date_c = '10/10/2010' AND h_de_debut > 0 AND id_a = 1 
+    -- on récupère les info du plus proche cours suivant
+    SELECT h_de_debut INTO heureApres FROM RESERVER NATURAL JOIN COURS 
+    WHERE date_c = dateCours AND h_de_debut > heureCours AND id_a = new.id_a
     ORDER BY h_de_debut LIMIT 1;
 
-    IF heureCours < heureAvant + dureeAvant AND heureAvant != NULL AND dureeAvant != NULL THEN
+    -- on verifie que le cours d'avant ne chevauche pas la reservation qu'on veut ajouter
+    IF heureAvant IS NOT NULL AND dureeAvant IS NOT NULL AND heureCours < heureAvant + dureeAvant  THEN
         SET mes = concat(mes,"Ajout impossible car il existe déjà un cours précédent sur le créneau ", heureCours, "-", heureCours+dureeCours,".");
         signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
     end if;
-    IF heureCours + dureeCours > heureApres AND heureApres != NULL THEN
+    -- on verifie que le cours d'après ne chevauche pas la reservation qu'on veut ajouter
+    IF heureApres IS NOT NULL AND heureCours + dureeCours > heureApres THEN
         SET mes = concat(mes,"Ajout impossible car il existe déjà un cours suivant sur le créneau ", heureCours, "-", heureCours+dureeCours,".");
         signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
     end if;
 end|
 delimiter ;
 
+--Trigger vérifiant si les cours d'un adhérant ne se chevauchent pas (update)
+
+delimiter |
+CREATE OR REPLACE TRIGGER pasDeCheuvauchementAdherantUpdate before update on RESERVER for each row
+begin
+    DECLARE heureAvant INT;
+    DECLARE heureApres INT;
+    DECLARE mes VARCHAR(100) default '';
+    DECLARE dureeAvant INT;
+    DECLARE heureCours INT;
+    DECLARE dureeCours INT;
+    DECLARE dateCours INT;
+
+    -- on recupère les donnée sur le cour
+    SELECT h_de_debut, duree, date_c INTO heureCours, dureeCours, dateCours FROM COURS WHERE id_c = new.id_c;
+    
+    -- on récupère les info du plus proche cours précédent
+    SELECT h_de_debut,duree INTO heureAvant, dureeAvant FROM RESERVER NATURAL JOIN COURS 
+    WHERE date_c = dateCours AND h_de_debut < heureCours AND id_a = new.id_a
+    ORDER BY h_de_debut DESC LIMIT 1;
+
+    -- on récupère les info du plus proche cours suivant
+    SELECT h_de_debut INTO heureApres FROM RESERVER NATURAL JOIN COURS 
+    WHERE date_c = dateCours AND h_de_debut > heureCours AND id_a = new.id_a
+    ORDER BY h_de_debut LIMIT 1;
+
+    -- on verifie que le cours d'avant ne chevauche pas la reservation qu'on veut modifier
+    IF heureAvant IS NOT NULL AND dureeAvant IS NOT NULL AND heureCours < heureAvant + dureeAvant  THEN
+        SET mes = concat(mes,"Ajout impossible car il existe déjà un cours précédent sur le créneau ", heureCours, "-", heureCours+dureeCours,".");
+        signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+    end if;
+    -- on verifie que le cours d'après ne chevauche pas la reservation qu'on veut modifier
+    IF heureApres IS NOT NULL AND heureCours + dureeCours > heureApres THEN
+        SET mes = concat(mes,"Ajout impossible car il existe déjà un cours suivant sur le créneau ", heureCours, "-", heureCours+dureeCours,".");
+        signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+    end if;
+end|
+delimiter ;
 
 -- insert temporaires de test
 
@@ -118,6 +161,7 @@ INSERT into MONITEUR VALUES
 insert into COURS(id_c,id_m,nb_pe,h_de_debut,duree,date_c) values (1,1,10,15,1,NOW());
 insert into COURS(id_c,id_m,nb_pe,h_de_debut,duree,date_c) values(2,1,3,17,1,NOW());
 insert into COURS(id_c,id_m,nb_pe,h_de_debut,duree,date_c) values (3,2,9,14,2, NOW());
+insert into COURS(id_c,id_m,nb_pe,h_de_debut,duree,date_c) values (4,1,9,15,2, NOW());
 
 insert into RESERVER(id_a,id_po,id_c) values(1,2,1);
 insert into RESERVER(id_a,id_po,id_c) values(2,1,1);
@@ -126,3 +170,4 @@ insert into RESERVER(id_a,id_po,id_c) values(2,1,2);
 insert into RESERVER(id_a,id_po,id_c) values(4,3,2);
 insert into RESERVER(id_a,id_po,id_c) values(5,4,2);
 INSERT INTO RESERVER VALUES (1,2,3);
+INSERT INTO RESERVER VALUES (1,2,4);

01a2b61af8aff1f9b4c8a364c02855ea77255a05
Loris-Gr <grandchamploris@gmail.com>
2025-01-09 12:50:33 +0100

    correction du model

--- project/models.py
+++ project/models.py
@@ -7,31 +7,32 @@ class Poney(db.Model):
     charge_max = db.Column(db.Numeric(4, 2))
     reservations = db.relationship('Reserver', back_populates='poney')
 
-class Adherent(db.Model):
-    __tablename__ = 'ADHERENT'
-    id_a = db.Column(db.Integer, primary_key=True)
-    nom_a = db.Column(db.String(42))
-    prenom_a = db.Column(db.String(42))
+class User(db.Model):
+    __tablename__ = 'USER'
+    id_u = db.Column(db.Integer, primary_key=True)
+    nom_u = db.Column(db.String(42))
+    prenom_u = db.Column(db.String(42))
     date_de_naissance = db.Column(db.Date)
     poids = db.Column(db.Numeric(4, 2))
     email = db.Column(db.String(42))
     mdp = db.Column(db.String(42))
-    cotisations = db.relationship('Cotiser', back_populates='adherent')
-    reservations = db.relationship('Reserver', back_populates='adherent')
+    le_role = db.Column(db.Enum("admin", "adherent", "moniteur"))
+    cotisations = db.relationship('Cotiser', back_populates='user')
+    reservations = db.relationship('Reserver', back_populates='user')
 
 class Reserver(db.Model):
     __tablename__ = 'RESERVER'
-    id_a = db.Column(db.Integer, db.ForeignKey('ADHERENT.id_a'), primary_key=True)
+    id_u = db.Column(db.Integer, db.ForeignKey('USER.id_u'), primary_key=True)
     id_po = db.Column(db.Integer, db.ForeignKey('PONEY.id_po'), primary_key=True)
     id_c = db.Column(db.Integer, db.ForeignKey('COURS.id_c'), primary_key=True)
-    adherent = db.relationship('Adherent', back_populates='reservations')
+    adherent = db.relationship('User', back_populates='reservations')
     poney = db.relationship('Poney', back_populates='reservations')
     cours = db.relationship('Cours', back_populates='reservations')
 
 class Cours(db.Model):
     __tablename__ = 'COURS'
     id_c = db.Column(db.Integer, primary_key=True)
-    id_m = db.Column(db.Integer, db.ForeignKey('MONITEUR.id_m'))
+    id_m = db.Column(db.Integer, db.ForeignKey('User.id_u'))
     nb_pe = db.Column(db.Integer, check_constraint="nb_pe <= 10 AND nb_pe >= 1")
     h_de_debut = db.Column(db.Integer)
     duree = db.Column(db.Integer, check_constraint="duree = 1 OR duree = 2")
@@ -40,17 +41,17 @@ class Cours(db.Model):
     reservations = db.relationship('Reserver', back_populates='cours')
 
     # Relation avec Moniteur
-    moniteur = db.relationship('Moniteur', back_populates='cours')
+    moniteur = db.relationship('User', back_populates='cours')
 
 
 class Cotiser(db.Model):
     __tablename__ = 'COTISER'
-    id_a = db.Column(db.Integer, db.ForeignKey('ADHERENT.id_a'), primary_key=True)
+    id_a = db.Column(db.Integer, db.ForeignKey('USER.id_u'), primary_key=True)
     annee_debut = db.Column(db.Integer, db.ForeignKey('COTISATION.annee_debut'), primary_key=True)
     annee_fin = db.Column(db.Integer, db.ForeignKey('COTISATION.annee_fin'), primary_key=True)
     paye = db.Column(db.Boolean)
 
-    adherent = db.relationship('Adherent', back_populates='cotisations')
+    adherent = db.relationship('User', back_populates='cotisations')
     cotisation = db.relationship(
         'Cotisation',
         back_populates='cotisants',
@@ -77,19 +78,6 @@ class Cotisation(db.Model):
     )
 
 
-
-class Moniteur(db.Model):
-    __tablename__ = 'MONITEUR'
-    id_m = db.Column(db.Integer, primary_key=True)
-    nom_m = db.Column(db.String(42))
-    prenom_m = db.Column(db.String(42))
-    date_de_naissance = db.Column(db.Date)
-    poids = db.Column(db.Numeric(4, 2))
-    email = db.Column(db.String(42))
-    mdp = db.Column(db.String(42))
-    cours = db.relationship('Cours', back_populates='moniteur')
-
-
 @login_manager.user_loader
 def load_user(user_id):
-    return Adherent.query.get(int(user_id))
+    return User.query.get(int(user_id))

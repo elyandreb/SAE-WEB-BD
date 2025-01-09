99c2aad1e8039bbbbd8e06e54dc5b733d2ea6ca9
elyandreb <elyandreburet72@gmail.com>
2025-01-09 17:27:13 +0100

    page accueil moniteur V1

--- project/views/accueil.py
+++ project/views/accueil.py
@@ -16,7 +16,8 @@ def accueil(adherent_id):
         return "Adhérent introuvable", 404
 
     if adherent.le_role == "admin":
-        return render_template("admin_home.html", adherent = adherent)
+        return render_template("admin_home.html", adherent=adherent)
+    
     # Trouver le prochain cours réservé par cet adhérent
     prochain_cours = (
         db.session.query(Cours)
@@ -25,4 +26,15 @@ def accueil(adherent_id):
         .order_by(Cours.date_c.asc())
         .first()
     )
-    return render_template('home.html', adherent=adherent, cours=prochain_cours)
+
+    if adherent.le_role == "moniteur":
+        today = date.today()
+        prochains_cours = (
+            Cours.query.filter(Cours.id_u == adherent.id_u, Cours.date_c >= today)
+            .order_by(Cours.date_c.asc(), Cours.h_de_debut.asc())
+            .limit(3)
+            .all()
+        )
+        return render_template("moniteur_home.html", adherent=adherent, prochains_cours=prochains_cours)
+    else:
+        return render_template('adherent_home.html', adherent=adherent, cours=prochain_cours)

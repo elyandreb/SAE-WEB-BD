87d81811cd23754f77ae1ba47345bcab73dd231e
elyandreb <elyandreburet72@gmail.com>
2025-01-09 13:46:44 +0100

    maj model

--- project/models.py
+++ project/models.py
@@ -1,4 +1,5 @@
 from .app import db, login_manager
+from flask_login import UserMixin
 
 class Poney(db.Model):
     __tablename__ = 'PONEY'
@@ -7,7 +8,7 @@ class Poney(db.Model):
     charge_max = db.Column(db.Numeric(4, 2))
     reservations = db.relationship('Reserver', back_populates='poney')
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'USER'
     id_u = db.Column(db.Integer, primary_key=True)
     nom_u = db.Column(db.String(42))
@@ -19,39 +20,38 @@ class User(db.Model):
     le_role = db.Column(db.Enum("admin", "adherent", "moniteur"))
     cotisations = db.relationship('Cotiser', back_populates='user')
     reservations = db.relationship('Reserver', back_populates='user')
+    cours = db.relationship('Cours', back_populates='moniteur')
 
 class Reserver(db.Model):
     __tablename__ = 'RESERVER'
     id_u = db.Column(db.Integer, db.ForeignKey('USER.id_u'), primary_key=True)
     id_po = db.Column(db.Integer, db.ForeignKey('PONEY.id_po'), primary_key=True)
     id_c = db.Column(db.Integer, db.ForeignKey('COURS.id_c'), primary_key=True)
-    adherent = db.relationship('User', back_populates='reservations')
+    user = db.relationship('User', back_populates='reservations')
     poney = db.relationship('Poney', back_populates='reservations')
     cours = db.relationship('Cours', back_populates='reservations')
 
 class Cours(db.Model):
     __tablename__ = 'COURS'
     id_c = db.Column(db.Integer, primary_key=True)
-    id_m = db.Column(db.Integer, db.ForeignKey('User.id_u'))
+    id_u = db.Column(db.Integer, db.ForeignKey('USER.id_u'))  # Moniteur
     nb_pe = db.Column(db.Integer, check_constraint="nb_pe <= 10 AND nb_pe >= 1")
     h_de_debut = db.Column(db.Integer)
     duree = db.Column(db.Integer, check_constraint="duree = 1 OR duree = 2")
     date_c = db.Column(db.Date)
     prix = db.Column(db.Numeric(5, 2))
     reservations = db.relationship('Reserver', back_populates='cours')
-
-    # Relation avec Moniteur
     moniteur = db.relationship('User', back_populates='cours')
 
 
 class Cotiser(db.Model):
     __tablename__ = 'COTISER'
-    id_a = db.Column(db.Integer, db.ForeignKey('USER.id_u'), primary_key=True)
+    id_u = db.Column(db.Integer, db.ForeignKey('USER.id_u'), primary_key=True)
     annee_debut = db.Column(db.Integer, db.ForeignKey('COTISATION.annee_debut'), primary_key=True)
     annee_fin = db.Column(db.Integer, db.ForeignKey('COTISATION.annee_fin'), primary_key=True)
     paye = db.Column(db.Boolean)
+    user = db.relationship('User', back_populates='cotisations')
 
-    adherent = db.relationship('User', back_populates='cotisations')
     cotisation = db.relationship(
         'Cotisation',
         back_populates='cotisants',

66738db531a1e5f8eb6a4dc01fcb5a7b53df03a3
Loris-Gr <grandchamploris@gmail.com>
2025-01-09 22:08:25 +0100

    légères modifications essentielles

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 from .app import app, db
 import project.views.accueil
 import project.views.authentification
+import project.views.profil
 import project.commands
 import project.models
--- project/views/profil.py
+++ project/views/profil.py
@@ -2,7 +2,7 @@ from hashlib import sha256
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import date
 from flask_wtf import FlaskForm
-from flask_login import login_required
+from flask_login import login_required, current_user
 from project.app import db, app
 from project.models import  Utilisateur, Cotiser
 from wtforms import StringField, PasswordField, EmailField, DateField, HiddenField, DecimalField, SubmitField
@@ -49,55 +49,55 @@ MOIS = {
         12: "décembre"
     }
 
-@app.route('/profil/<int:utilisateur_id>',methods=["GET", "POST"])
-def profil(utilisateur_id) :
+@app.route("/profil/",methods=["GET", "POST"])
+@login_required
+def profil() :
     f = ProfileForm()
     mdp_f = ChangeMdpForm()
 
     edit = request.args.get('edit', False, type=bool)
 
-    utilisateur = Utilisateur.query.get(utilisateur_id)
-
-    if not utilisateur:
-        return "Utilisateur introuvable", 404
+    utilisateur_id = current_user.get_id()
     
     jour = date.today() # date actuelle
     annee = jour.year
 
     if date(annee, 9, 1) < jour < date(annee, 12, 31) :
         cotisation = Cotiser.query.filter_by(id_u=utilisateur_id, annee_debut=annee, annee_fin=annee+1).first()
+    else : 
+        cotisation = Cotiser.query.filter_by(id_u=utilisateur_id, annee_debut=annee-1, annee_fin=annee).first()
     
     jour_str = f"Le {jour.day:02d} {MOIS[jour.month]} {jour.year}"
 
 
     if request.method == 'GET':
-        f.id_u.data = utilisateur.id_u
-        f.name.data = utilisateur.nom_u
-        f.first_name.data = utilisateur.prenom_u
-        f.birth_date.data = utilisateur.date_de_naissance
-        f.email.data = utilisateur.email
-        f.poids.data = utilisateur.poids
+        f.id_u.data = current_user.id_u
+        f.name.data = current_user.nom_u
+        f.first_name.data = current_user.prenom_u
+        f.birth_date.data = current_user.date_de_naissance
+        f.email.data = current_user.email
+        f.poids.data = current_user.poids
 
     if request.method == 'POST':
         if 'save_profile' in request.form and f.validate():
-            utilisateur.nom_u = f.name.data
-            utilisateur.prenom_u = f.first_name.data
-            utilisateur.date_de_naissance = f.birth_date.data
-            utilisateur.email = f.email.data
-            utilisateur.poids = f.poids.data
+            current_user.nom_u = f.name.data
+            current_user.prenom_u = f.first_name.data
+            current_user.date_de_naissance = f.birth_date.data
+            current_user.email = f.email.data
+            current_user.poids = f.poids.data
             db.session.commit()
             flash("Profil mis à jour avec succès", "success")
-            return redirect(url_for("client_profil"))
+            return redirect(url_for("profil"))
 
         if 'change_password' in request.form and mdp_f.validate():
             old_hash = sha256(mdp_f.old_password.data.encode()).hexdigest()
-            if old_hash != utilisateur.mdp:
+            if old_hash != current_user.mdp:
                 flash("Ancien mot de passe incorrect", "danger")
             else:
                 new_hash = sha256(mdp_f.new_password.data.encode()).hexdigest()
-                utilisateur.mdp = new_hash
+                current_user.mdp = new_hash
                 db.session.commit()
                 flash("Mot de passe mis à jour avec succès", "success")
-                return redirect(url_for("client_profil"))
+                return redirect(url_for("profil"))
 
-    return render_template("profil.html", form=f, edit=edit, mdp_f=mdp_f, cotisation = cotisation, jour = jour_str)
+    return render_template("profil.html", form=f, edit=edit, mdp_f=mdp_f, cotisation = cotisation, jour = jour_str, utilisateur = current_user )

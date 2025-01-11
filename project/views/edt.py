53981d415d9d5b4e843441cc541ebffbea7186f7
elyandreb <elyandreburet72@gmail.com>
2025-01-11 17:28:34 +0100

    emploie du temps moniteur fonctionnel V1

---
+++ project/views/edt.py
@@ -0,0 +1,40 @@
+from flask import render_template, request
+from flask_login import login_required, current_user
+from datetime import date, timedelta
+from project.models import Cours
+from project import app, db
+
+@app.route('/edt')
+def emploi_du_temps():
+    # Vérifiez si l'utilisateur est un moniteur
+    print(current_user.le_role)
+    if current_user.le_role == "moniteur":
+
+        # Récupérer la semaine à afficher
+        today = date.today()
+        semaine_index = int(request.args.get('week', 0))  # Décalage de la semaine
+        semaine_debut = today + timedelta(weeks=semaine_index, days=-today.weekday())
+        semaine_fin = semaine_debut + timedelta(days=6)
+
+        # Générer les jours de la semaine
+        jours = [semaine_debut + timedelta(days=i) for i in range(7)]
+
+        # Récupérer les cours de la semaine pour le moniteur
+        cours_semaine = (
+            Cours.query.filter(
+                Cours.id_u == current_user.id_u,
+                Cours.date_c >= semaine_debut,
+                Cours.date_c <= semaine_fin
+            )
+            .order_by(Cours.date_c, Cours.h_de_debut)
+            .all()
+        )
+
+        # Générer les horaires
+        horaires = range(9, 21)  # De 9h à 21h
+
+        return render_template('edt_moniteur.html', semaine_debut=semaine_debut, jours=jours, horaires=horaires, cours_semaine=cours_semaine, previous_week=semaine_index - 1, next_week=semaine_index + 1, utilisateur=current_user)
+
+
+
+
